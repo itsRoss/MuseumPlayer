e05ee87049a6397a930e7939a6ce2cf26cf5053d
Davide Rossignolo <55827662+itsRoss@users.noreply.github.com>
2020-09-21 13:28:54 +0200

    final version

--- playerMuseo.py
+++ playerMuseo.py
@@ -1,56 +1,72 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 import vlc
 import time
 from gpiozero import Button
+from vlc import EventType
 
-#create mediaPlayer object
-player = vlc.MediaPlayer('/home/pi/Desktop/VlcPlayer/Videos/1.mp4')
-	
+vlcInstance = None
+player = None
+media = None
+fadeSpeed = 0.05
+premuto = False
+# def fadeVolume():
+    # vol = player.audio_get_volume()
+    # while vol != 0:
+        # vol -= 1
+        # #setvolume returns -1 if vol is out of range. you never know
+        # if player.audio_set_volume(vol) == -1:
+            # vol = 0
+            # player.audio_set_volume(vol)
+        # time.sleep(fadeSpeed)
 
-#on release of the button	
-def released(button):
-
-    global Player
-
-    if button.pin.number == 5:
-		#set position to black 
-        player.set_position(0.0)
-        #pause on
-        player.set_pause(1)
-        time.sleep(0.2)
-        
-
-#on press of the button
-def pressed(button):
-	
-    global Player
-
-    if button.pin.number == 5:
-		#resume the video (pause off)
-        player.set_pause(0)
-    #stop player
-    elif button.pin.number == 20:
-		player.stop()
+def resetPlayer():
+    player.set_pause(1)
+    player.set_position(0.015)
+    #player.audio_set_volume(100)
 
 
+def pressed():
+    global premuto
+    premuto = True
+    print('premuto')
+    resetPlayer()
+    player.set_pause(0)
+    
+
+def inPause(event):
+    global premuto
+    if(premuto==False):
+	player.set_position(0.0)
+	player.set_pause(1)
+    else:
+	premuto = False
+    
+    
 if __name__ == '__main__':
-	
-	#defining button and methods used by the button
-    button5 = Button(5)
-    button5.when_pressed = pressed
-    button5.when_released = released
     
-    button20 = Button(20)
-    button20.when_pressed = pressed
-    
-	#initialize video on black position and pause
+    button27 = Button(27)
+    button27.when_pressed = pressed
+
+    vlcInstance = vlc.Instance()
+
+    media = vlcInstance.media_new_path('Videos/Museoh265.mp4')
+    media.add_option(':play-and-pause')
+
+    player = vlcInstance.media_player_new()
+    #player.set_fullscreen(True)
+    player.set_media(media)
     player.play()
-    player.set_fullscreen(True)
     time.sleep(0.5)
     player.set_pause(1)
+    
+    # #event end
+    event_manager = player.event_manager()
+    event_manager.event_attach(vlc.EventType.MediaPlayerPaused, inPause)
+	
+
+    
+    
 
     while True:
         continue
+
 
