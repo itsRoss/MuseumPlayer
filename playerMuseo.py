8735dabd22f11422b8f03c7ad76dead392690228
Davide Rossignolo <55827662+itsRoss@users.noreply.github.com>
2020-09-25 10:27:37 +0200

    Add files via upload

--- playerMuseo.py
+++ playerMuseo.py
@@ -3,59 +3,56 @@ import time
 from gpiozero import Button
 from vlc import EventType
 
-# vlcInstance = None
-# player = None
-# media = None
-# premuto = None	#used to control preload
+vlcInstance = None
+player = None
+media = None
+premuto = False	#used to control preload
 
 
-# #reset player when pressed
-# def resetPlayer():
-    # player.set_pause(1)
-    # player.set_position(0.015)
-    # #player.audio_set_volume(100)
+#reset player when pressed
+def resetPlayer():
+    #player.set_pause(1)
+    player.set_position(0.015)
+    #player.audio_set_volume(100)
 
 #pressed button
 def pressed():
-    # global premuto
-    # premuto = True
-    # resetPlayer()
-    # player.set_pause(0)
+    global premuto
+    #premuto = True
+    resetPlayer()
+    player.set_pause(0)
     print('press')
 #place at start when ends
-# def inPause(event):
-    # global premuto
-    
-    # if(premuto==False):
-	# player.set_position(0.0)
-	# player.set_pause(1)
-	
-	
-    # else:
-	# premuto = False
+def inPause(event):
+    global premuto
+    print(premuto)
+    if(premuto==False):
+	player.set_pause(1)	
+	player.set_position(0.0)
+		
+    else:
+	premuto = False
     
     
 if __name__ == '__main__':
-    
+   
     button17 = Button(17)
     button17.when_pressed = pressed
     
-    # #create instance
-    # vlcInstance = vlc.Instance()
-    # media = vlcInstance.media_new_path('Videos/museo_press.mp4')
-    # media.add_option(':play-and-pause')
-    # player = vlcInstance.media_player_new()
-    # #player.set_fullscreen(True)
-    # player.set_media(media)
-    # player.play()
-    # player.set_pause(1)
-    # player.set_position(0.0)
+    #create instance
+    vlcInstance = vlc.Instance()
+    media = vlcInstance.media_new_path('Videos/museo_press.mp4')
+    media.add_option(':play-and-pause')
+    player = vlcInstance.media_player_new()
+    #player.set_fullscreen(True)
+    player.set_media(media)
+    player.play()
+    player.set_pause(1)
+    player.set_position(0.0)
     
-    # #end video
-    # event_manager = player.event_manager()
-    # event_manager.event_attach(vlc.EventType.MediaPlayerPaused, inPause)
-	
-    
+    #end video
+    event_manager = player.event_manager()
+    event_manager.event_attach(vlc.EventType.MediaPlayerPaused, inPause)
 
     while True:
         continue
